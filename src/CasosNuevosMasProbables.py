87876d79ba104c79f46b8e84baf985059749b418
EnzoAndree <EnzoAndree@users.noreply.github.com>
2021-06-11 22:53:09 -0400

    Update CasosNuevosMasProbables

--- src/CasosNuevosMasProbables.py
+++ src/CasosNuevosMasProbables.py
@@ -10,7 +10,7 @@ outputdir.mkdir(parents=True, exist_ok=True)
 dp5 = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales_T.csv', index_col=0)
 dp5['Casos nuevos probables'] = dp5['Casos probables acumulados'].diff()
 casos = dp5[['Casos nuevos totales']].copy()
-casos['Casos nuevos totales + antigeno + probables'] = dp5[['Casos nuevos totales', 'Casos nuevos confirmados por antigeno', 'Casos nuevos probables']].sum(axis=1)
+casos['Casos nuevos totales + probables'] = dp5[['Casos nuevos totales', 'Casos nuevos probables']].sum(axis=1)
 fig = go.Figure()
 Wong = ['#000000', '#E69F00', '#56B4E9',
         '#009E73', '#F0E442', '#0072B2',
@@ -25,14 +25,14 @@ fig.add_trace(
 )
 fig.add_trace(
     go.Scatter(x=casos.index,
-               y=casos['Casos nuevos totales + antigeno + probables'],
+               y=casos['Casos nuevos totales + probables'],
                mode='lines',
-               name='Casos nuevos totales + antigeno + probables (DP5)',
+               name='Casos nuevos totales + probables (DP5)',
                line_color=Wong[1]
               )
 )
 fig.update_layout(hovermode='x')
 fig.update_layout(template='plotly_white',
-                  title='Comparación de los casos nuevos totales con los casos probables y casos positivos por test de antígeno')
+                  title='Comparación de los casos nuevos totales con los casos probables')
 fig.update_layout(yaxis_tickformat = ',')
 fig.write_html(f'{outputdir}/Casos_Nuevos_Mas_Probables.html')

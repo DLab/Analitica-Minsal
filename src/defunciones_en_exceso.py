6dab305cc5388eb22d21763325fa3b9b1f7bb77e
Enzo Guerrero-Araya <EnzoAndree@users.noreply.github.com>
2022-12-04 12:57:09 -0300

    update new dataset deis

--- src/defunciones_en_exceso.py
+++ src/defunciones_en_exceso.py
@@ -25,7 +25,7 @@ import imageio
 
 
 def get_deis_death_url():
-    datapattern = compile('http.*DEFUNCIONES_FUENTE_DEIS_2016_.*zip\"\\n\"tags\":\"defunciones\"')
+    datapattern = compile('http.*DEFUNCIONES_FUENTE_DEIS.*zip\"\\n\"tags\":\"defunciones\"')
     with urllib.request.urlopen('https://deis.minsal.cl/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=2889&target_action=get-all-data&default_sorting=manual_sort') as f:
         return datapattern.search(f.read().decode().replace(',','\n')).group().replace('\\', '').split('"')[0]
 
@@ -144,8 +144,8 @@ if __name__ == "__main__":
     deis_data = Path('deis_data')
     deis_data.mkdir(parents=True, exist_ok=True)
     deis_csv = 'deis_data/' + get_csv_deis() + 'csv'
-    columnas = ['ANO_DEF', 'FECHA_DEF', 'GLOSA_SEXO', 'EDAD_TIPO', 'EDAD_CANT', 'CODIGO_COMUNA_RESIDENCIA', 'GLOSA_COMUNA_RESIDENCIA', 'GLOSA_REG_RES', 'DIAG1', 'CAPITULO_DIAG1', 'GLOSA_CAPITULO_DIAG1', 'CODIGO_GRUPO_DIAG1', 'GLOSA_GRUPO_DIAG1', 'CODIGO_CATEGORIA_DIAG1', 'GLOSA_CATEGORIA_DIAG1', 'CODIGO_SUBCATEGORIA_DIAG1', 'GLOSA_SUBCATEGORIA_DIAG1', 'DIAG2', 'CAPITULO_DIAG2', 'GLOSA_CAPITULO_DIAG2', 'CODIGO_GRUPO_DIAG2', 'GLOSA_GRUPO_DIAG2', 'CODIGO_CATEGORIA_DIAG2', 'GLOSA_CATEGORIA_DIAG2', 'CODIGO_SUBCATEGORIA_DIAG2', 'GLOSA_SUBCATEGORIA_DIAG2']
-    deis = pd.read_csv(deis_csv, sep=';', parse_dates=[1], header=None, names=columnas, index_col=False, encoding='latin-1')
+    #columnas = ['ANO_DEF', 'FECHA_DEF', 'GLOSA_SEXO', 'EDAD_TIPO', 'EDAD_CANT', 'CODIGO_COMUNA_RESIDENCIA', 'GLOSA_COMUNA_RESIDENCIA', 'GLOSA_REG_RES', 'DIAG1', 'CAPITULO_DIAG1', 'GLOSA_CAPITULO_DIAG1', 'CODIGO_GRUPO_DIAG1', 'GLOSA_GRUPO_DIAG1', 'CODIGO_CATEGORIA_DIAG1', 'GLOSA_CATEGORIA_DIAG1', 'CODIGO_SUBCATEGORIA_DIAG1', 'GLOSA_SUBCATEGORIA_DIAG1', 'DIAG2', 'CAPITULO_DIAG2', 'GLOSA_CAPITULO_DIAG2', 'CODIGO_GRUPO_DIAG2', 'GLOSA_GRUPO_DIAG2', 'CODIGO_CATEGORIA_DIAG2', 'GLOSA_CATEGORIA_DIAG2', 'CODIGO_SUBCATEGORIA_DIAG2', 'GLOSA_SUBCATEGORIA_DIAG2']
+    deis = pd.read_csv(deis_csv, sep=';', parse_dates=[1], index_col=False, encoding='latin-1')
     deis.set_index(['FECHA_DEF'], inplace=True)
     deis.sort_index(inplace=True)
     # CODIGO_CATEGORIA_DIAG1 U07 > covid19
@@ -153,23 +153,24 @@ if __name__ == "__main__":
     lastupdatedeis = open(f'{outputdir}/updatedeis.log', 'r').readlines()[0]
     if lastupdatedeis.strip() != deis.index[-1].strftime('%d/%m/%Y'):
         deis['EDAD_ANOS'] = deis.apply(annos, axis = 1)
-        deis['ANO_DEF'] = deis['ANO_DEF'].astype('int32')
+        deis['AÑO'] = deis['AÑO'].astype('int32')
         bins = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 999]
         bins_10s = [0, 10, 20, 30, 40, 50, 60, 70, 80, 999]
         labels = ['00-04', '05-09', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80+']
         labels_10s = ['00-09', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
         deis['agerange'] = pd.cut(deis.EDAD_ANOS, bins, labels=labels, include_lowest=True, right=False)
         deis['agerange_10s'] = pd.cut(deis.EDAD_ANOS, bins_10s, labels=labels_10s, include_lowest=True, right=False)
+        deis = deis[deis.index.notnull()]
         # default figure sizes
         figsize = (10, 5)
 
         # create a list of month strings, for plotting purposes
         month_strings = calendar.month_name[1:]
 
-        deis_gruped = pd.pivot_table(deis, values=['EDAD_CANT'], index=['FECHA_DEF'],
-                            columns=['GLOSA_SEXO', 'agerange_10s'], aggfunc='count')['EDAD_CANT'].resample('W-Mon').sum()
-        deis_prepandemia = pd.pivot_table(deis.loc[(deis['ANO_DEF'] <= 2019)], values=['EDAD_CANT'], index=['FECHA_DEF'],
-                            columns=['GLOSA_SEXO', 'agerange_10s'], aggfunc='count')['EDAD_CANT'].resample('W-Mon').sum()
+        deis_gruped = pd.pivot_table(deis.loc[(deis['AÑO'] >= 2000)], values=['EDAD_CANT'], index=['FECHA_DEF'],
+                            columns=['SEXO_NOMBRE', 'agerange_10s'], aggfunc='count')['EDAD_CANT'].resample('W-Mon').sum()
+        deis_prepandemia = pd.pivot_table(deis.loc[(deis['AÑO'] <= 2019) & (deis['AÑO'] >= 2000)], values=['EDAD_CANT'], index=['FECHA_DEF'],
+                            columns=['SEXO_NOMBRE', 'agerange_10s'], aggfunc='count')['EDAD_CANT'].resample('W-Mon').sum()
         deis_gruped = deis_gruped.sum(axis=1).iloc[1:-2]#.groupby(pd.Grouper(freq='W')).sum().iloc[1:-1]
         deis_prepandemia = deis_prepandemia.sum(axis=1).iloc[1:-1]#.groupby(pd.Grouper(freq='W')).sum().iloc[1:-1]
 
@@ -185,9 +186,9 @@ if __name__ == "__main__":
             time = pm.MutableData("time", np.array(list(range(len(deis_prepandemia)))), dims='t')
             deaths = pm.MutableData("deaths", deis_prepandemia.to_numpy(), dims='t')
             # priors
-            intercept = pm.Normal("intercept", 2200, 10)
+            intercept = pm.Normal("intercept", 1629, 10)
             month_mu = ZeroSumNormal("month mu", sigma=200, dims="month")
-            linear_trend = pm.TruncatedNormal("linear trend", 0, 0.1, lower=0)
+            linear_trend = pm.TruncatedNormal("linear trend", 0.5, 0.1, lower=0)
 
             # the actual linear model
             mu = pm.Deterministic(
@@ -261,9 +262,9 @@ if __name__ == "__main__":
 
         plt.figtext(0.5, -0.01, NOTAS[0], ha="center", bbox={"facecolor":"lightgray", "alpha":0.5, "pad":5}, fontsize='small')
 
-        ax[0].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
-        ax[1].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
-        ax[2].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
+        ax[0].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
+        ax[1].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
+        ax[2].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
 
 
         img = imageio.v2.imread('https://i2.wp.com/dlab.cl/wp-content/uploads/2016/08/LogoWebDlab.png')
@@ -283,7 +284,7 @@ if __name__ == "__main__":
 
         fig, ax = plt.subplots(3, 1, figsize=(figsize[0]+5, 9), sharex=True, gridspec_kw={'height_ratios': [5, 2, 2]})
         #az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.5, smooth=True)
-        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':11}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
+        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':3}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
 
         ax[0].plot(humanweek.index, humanweek, label="Defunciones")
         #format_x_axis(ax)
@@ -309,9 +310,9 @@ if __name__ == "__main__":
 
         plt.figtext(0.5, -0.01, NOTAS[0], ha="center", bbox={"facecolor":"lightgray", "alpha":0.5, "pad":5}, fontsize='small')
 
-        ax[0].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
-        ax[1].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
-        ax[2].set_xlim(xmin=datetime.datetime(2016, 1, 1, ))
+        ax[0].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
+        ax[1].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
+        ax[2].set_xlim(xmin=datetime.datetime(2000, 1, 1, ))
 
         # put a new axes where you want the image to appear
         # (x, y, width, height)
@@ -329,7 +330,7 @@ if __name__ == "__main__":
 
         fig, ax = plt.subplots(3, 1, figsize=(figsize[0]+5, 9), sharex=True, gridspec_kw={'height_ratios': [5, 2, 2]})
         #az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.5, smooth=True)
-        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':11}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
+        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':3}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
 
         ax[0].plot(humanweek.index, humanweek, label="Defunciones")
         #format_x_axis(ax)
@@ -373,7 +374,7 @@ if __name__ == "__main__":
 
         fig, ax = plt.subplots(3, 1, figsize=(figsize[0]+5, 9), sharex=True, gridspec_kw={'height_ratios': [5, 2, 2]})
         #az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.5, smooth=True)
-        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':11}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
+        az.plot_hdi(mdates.date2num(humanweek.index), completecounterfactual.posterior_predictive["obs"], hdi_prob=0.95, smooth=True, smooth_kwargs={'window_length':3}, fill_kwargs={"label": "Modelo (95% Confianza)"}, color="C2", ax = ax[0])
 
         ax[0].plot(humanweek.index, humanweek, label="Defunciones")
         #format_x_axis(ax)
